acbbb4aafe157f896ffe7116780e8473e0939ab7
rfeinberg3 <rfeinberg3@gmail.com>
2024-06-12 22:49:39 -0400

    Update READMEs and small code updates

--- DataScraper/src/main.py
+++ DataScraper/src/main.py
@@ -1,4 +1,4 @@
-import os, sys
+import sys
 sys.path.append('..')
 import json
 from src.scraper import scraper
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     for keyword in keywords:
         # Max limit is 200 and offset must be a multiple of limit or error will occur. deep_search=True helps to reduce search results that aren't related to the keyword.
         for data_dump in datascraper.search_and_scrape(keyword, limit='200', offset='0', deep_search=True): 
-            with open(f'../outputs/{keyword.lower()}_data.json', 'a') as outfile:
+            with open(f'../outputs/{keyword.lower().replace('\n', '')}_data.json', 'a') as outfile:
                 json.dump(data_dump, outfile)
                 outfile.write("\n")
