92602b3e6c046693739158faccfb69d528e3a4c8
rfeinberg3 <rfeinberg3@gmail.com>
2024-07-30 19:55:42 -0400

    update test file

--- tests/postgres/retrieve_data_table.py
+++ tests/postgres/retrieve_data_table.py
@@ -4,7 +4,8 @@ sys.path.append('../../data')
 import pandas as pd
 from dbmanager import DatabaseManager
 
-dbm = DatabaseManager(dbname="eassistant", user="postgres", password="1234", host="localhost", port=5431)
-df = dbm.table_to_pandas("clothing")
+if __name__ == '__main__':
+    dbm = DatabaseManager(dbname="eassistant", user="postgres", password="1234", host="localhost", port=5431)
+    df = dbm.table_to_pandas("clothing")
 
-df.to_json("dataset.json", indent=2)
+    df.to_json("dataset.json", indent=2)
