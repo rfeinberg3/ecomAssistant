f39248f0bbbfd1b465a6671ebbf2d15c1fffdcc6
rfeinberg3 <rfeinberg3@gmail.com>
2024-07-31 23:29:34 -0400

    restructure

--- tests/postgres/retrieve_data_table.py
+++ tests/postgres/retrieve_data_table.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     dbm = DatabaseManager(dbname="eassistant", user="postgres", password="1234", host="localhost", port=5431)
     df = dbm.table_to_pandas("clothing")
 
-    df[0:10000].to_json("dataset.json", indent=2)
+    df.to_json("dataset.json", indent=2)
